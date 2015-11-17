793b28f1ff3bc6e49aa1c6dfe56b36e794e4dcc0
jinkai.xu <xjksmile@gmail.com>
2015-11-17 08:23:22 +0800

    init the project.

---
+++ src/main/java/com/xjk/home/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.xjk.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Intro:
+ * Project: KlipHome
+ * Date:    11/15/15
+ * Author:  xujinkai
+ */
+@RequestMapping("/")
+@Controller
+public class IndexController {
+
+
+
+
+
+
+}
